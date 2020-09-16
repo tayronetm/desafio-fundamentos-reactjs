e1eafa2b5c0711188d63024afc3f7abff4a85978
Monteiro <tayrone.n.monteiro@accenture.com>
2020-09-16 09:59:46 -0300

    Finish

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,28 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
-
     try {
-      // await api.post('/transactions/import', data);
+      await Promise.all(
+        uploadedFiles.map(file => {
+          const data = new FormData();
+          data.append('file', file.file, file.name);
+          return api.post('/transactions/import', data);
+        }),
+      );
+
+      history.goBack();
     } catch (err) {
-      // console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const filesMapped: FileProps[] = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles([...uploadedFiles, ...filesMapped]);
   }
 
   return (
